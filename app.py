9ba940c5589c2b45679c5767ee18902016d08ff7
Shivani Yadav <shivaaniyadav31@gmail.com>
2025-07-18 17:10:54 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = 'supersecretmre'
+
+@app.route('/')
+def index():
+    return render_template('landing.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 5000, debug=True)
