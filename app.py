a7f69f14609b3b636a7421d08a542eae1be637cf
Shivani Yadav <shivaaniyadav31@gmail.com>
2025-07-20 22:11:47 +0530

    ..

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app.secret_key = 'supersecretmre'
 
 @app.route('/')
 def index():
-    return render_template('landing.html')
+    return render_template('index.html')
 
 @app.route('/register')
 def register():
